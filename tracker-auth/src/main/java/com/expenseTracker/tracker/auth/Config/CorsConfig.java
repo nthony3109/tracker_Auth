a9ce0333207e7f632350d53753f737755b214ca5
nthony3109 <chibuzoranthonyajibo@gmail.com>
2025-10-29 16:25:25 +0100

    cross origin config

---
+++ tracker-auth/src/main/java/com/expenseTracker/tracker/auth/Config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.expenseTracker.tracker.auth.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer crossOrigins() {
+        return  new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+             registry .addMapping("/**")
+                     .allowedOrigins("http://localhost:8081")
+                     .allowedMethods("POST","GET","PUT","DELETE")
+                     .allowedHeaders("*")
+                     .allowCredentials(true);
+            }
+        };
+    }
+}
