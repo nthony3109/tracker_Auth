4991d6c37fe589137986bdaefa937c96241adcb2
nthony3109 <chibuzoranthonyajibo@gmail.com>
2025-10-30 15:44:28 +0100

    to get wanted fields

---
+++ tracker-auth/src/main/java/com/expenseTracker/tracker/auth/DTO/LoginRes.java
@@ -0,0 +1,27 @@
+package com.expenseTracker.tracker.auth.DTO;
+
+public class LoginRes {
+    private  String token;
+    private  Long  userId;
+
+    public LoginRes(String token, Long userId) {
+        this.token = token;
+        this.userId = userId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
