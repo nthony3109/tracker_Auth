72a3946e9baefc2dc1790c1ea5679dd7a46aa8d5
nthony3109 <chibuzoranthonyajibo@gmail.com>
2025-11-07 17:49:06 +0100

    token refresher entity

--- tracker-auth/src/main/java/com/expenseTracker/tracker/auth/Model/Rtoken.java
+++ tracker-auth/src/main/java/com/expenseTracker/tracker/auth/Model/Rtoken.java
@@ -1,4 +1,31 @@
 package com.expenseTracker.tracker.auth.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "token_refresher")
 public class Rtoken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+       private   Long id;
+    @Column(nullable = false, unique = true)
+    private  String refreshToken;
+    @Column(nullable = false)
+    private LocalDateTime expiryDate;
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(name = "user_field_id", referencedColumnName = "id", nullable = false)
+    private UserField userField;
+
+    public  void setUserField(UserField user) {
+        this.userField = user;
+    }
+
 }
