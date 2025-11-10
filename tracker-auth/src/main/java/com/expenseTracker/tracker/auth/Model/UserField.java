7c77a3c4f2e780e34bdf904acc06edd3b09ad914
nthony3109 <chibuzoranthonyajibo@gmail.com>
2025-11-10 15:02:13 +0100

    updated

--- tracker-auth/src/main/java/com/expenseTracker/tracker/auth/Model/UserField.java
+++ tracker-auth/src/main/java/com/expenseTracker/tracker/auth/Model/UserField.java
@@ -1,4 +1,92 @@
 package com.expenseTracker.tracker.auth.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserField {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    private String firstname;
+    private String lastname;
+    private  String email;
+    private  String username;
+    private  String password;
+    private String phone;
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Rtoken> rtoken = new ArrayList<>();
+
+    //methods to manage relationship
+    public  void addRtoken(Rtoken token) {
+        rtoken.add(token);
+        token.setUserField(this);
+    }
+    public  void removeRtoken(Rtoken token) {
+        rtoken.remove(token);
+        token.setUserField(null);
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
