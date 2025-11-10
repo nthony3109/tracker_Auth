d0b2ade82fd079dbb35976d3a993d54246fb8e42
nthony3109 <chibuzoranthonyajibo@gmail.com>
2025-11-10 15:00:08 +0100

    updated

--- tracker-auth/src/main/java/com/expenseTracker/tracker/auth/repo/RtokenRepo.java
+++ tracker-auth/src/main/java/com/expenseTracker/tracker/auth/repo/RtokenRepo.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface RtokenRepo extends JpaRepository<Rtoken,Long> {
-   Optional<Rtoken> findByRtoken(String token);
+   Optional<Rtoken> findByRefreshToken(String token);
    void  deleteAllRtokenByUser(UserField user);
 }
